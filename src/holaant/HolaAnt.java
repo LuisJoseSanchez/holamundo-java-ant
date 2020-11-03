a5af0d08c95b27052824821ea1f7b6177f83c012
Luis José Sánchez <luisjoseprofe@gmail.com>
2020-11-03 16:38:13 +0100

    Probando un "Hola mundo" con Ant.

--- src/holaant/HolaAnt.java
+++ src/holaant/HolaAnt.java
@@ -15,7 +15,7 @@ public class HolaAnt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("¡Hola mundo!");
     }
     
 }
